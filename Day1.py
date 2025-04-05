cd2bcee162e4a84fa19a375429977fb3d1eb7c80
Aritra Chattopadhyay <aritrachattopadhyay200@gmail.com>
2025-04-05 13:15:42 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,13 @@
+#print hello world
+print("Hello World!")
+
+#show datatype
+name = "Aritra"
+age = 20
+height = 5.10
+is_Student = True
+print(type(name),type(age),type(height),type(is_Student))
+
+#enter and print your name
+name = input("Enter your name: ")
+print("Your name: ",name)
