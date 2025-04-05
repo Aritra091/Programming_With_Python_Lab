08a85397f6be79f1d4c46d9bf6e7f018a27cfa2b
Aritra Chattopadhyay <aritrachattopadhyay200@gmail.com>
2025-04-05 14:17:37 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -11,3 +11,26 @@ print(type(name),type(age),type(height),type(is_Student))
 #enter and print your name
 name = input("Enter your name: ")
 print("Your name: ",name)
+
+#perform addition, subtraction, multiplication, division and modulo
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+print("Modulo: ",mod)
+
+#check number is positive, negative or 0
+n = int(input("Enter a number: "))
+if (n<0):
+  print("Negative number")
+elif (n>0):
+  print("Positive number")
+else:
+  print("Zero")
